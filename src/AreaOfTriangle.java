a2bd42452fba0654e9a4f84c6789349f814eaaf3
harshitkumar13-cloud <harshitkumar13@gmail.com>
2025-12-16 00:48:38 +0530

    This is my program for finding area of a triangle

---
+++ src/AreaOfTriangle.java
@@ -0,0 +1,9 @@
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 10;
+        double area = (0.5 * a * b);
+        int area2 = (int)area;
+        System.out.println(area2);
+    }
+}
--- src/MultiplyTwoInt.java
+++ src/MultiplyTwoInt.java
@@ -1,10 +1,13 @@
 public class MultiplyTwoInt {
-    public static void main (String[] args)
-    {
-        int a = 50;
-        int b = 25;
-        int c = (a*b);
-        System.out.println(c);
+    public static void main(String[] args) {
 
+
+        {
+            int a = 50;
+            int b = 25;
+            int c = (a * b);
+            System.out.println(c);
+
+        }
     }
 }
