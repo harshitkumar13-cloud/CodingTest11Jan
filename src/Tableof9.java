1dab1d66de30ae5ff1521c964a3953d8e0d15309
harshitkumar13-cloud <harshitkumar13@gmail.com>
2025-12-18 23:49:07 +0530

    This is my program for printing table of 9 using printf

---
+++ src/Tableof9.java
@@ -0,0 +1,10 @@
+public class Tableof9 {
+    public static void main(String[] args) {
+        int a = 9;
+        for(int i = 1; i<=10; i++)
+        {
+            int b = (a*i);
+            System.out.printf("%d * %d = %d\n", a, i, b);
+        }
+    }
+}
